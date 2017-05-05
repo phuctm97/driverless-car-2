e6e8bf19f1eb6a1f17809553900f5c210c6bef6a
phuctm97 <phuctm97@gmail.com>
2017-05-05 15:13:20 +0700

    Backup

--- DriverlessCar/Analyzer/AnalyzerCasesBased/CasesResolver.cpp
+++ DriverlessCar/Analyzer/AnalyzerCasesBased/CasesResolver.cpp
@@ -1,10 +1,9 @@
 #include "CasesResolver.h"
+#include "Cases/BothLaneCase.h"
 
 void sb::CasesResolver::predictCases( CaseRepository* caseRepository, CollectData* collectData, CalculateData* calculateData, std::vector<ICase*>& possibleCases )
 {
 	// check for data in repository, see how lane position and shape change
 
-	if ( caseRepository->getAll().empty() ) {
-		
-	}
+
 }
